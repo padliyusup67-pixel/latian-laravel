74268bccca1ca237746ad01ee96b506b30a4a864
Padli <padliyusup67@gmail.com>
2025-10-21 14:44:20 +0700

    perbaikan dashboard

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,7 @@
+<x-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-900 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+</x-layout>
