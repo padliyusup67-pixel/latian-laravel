c4904fcd245ca71a5f811686e8e27e7a13444087
Padli <padliyusup67@gmail.com>
2025-10-21 14:49:13 +0700

    update fitur-b: ubah tampilan dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,6 +1,6 @@
 <x-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-900 leading-tight">
+        <h2 class="font-bold text-2xl text-blue-900 leading-tight">
             {{ __('Dashboard') }}
         </h2>
     </x-slot>
